3f3fd6fef4410d65360a595fc0a6d9c3a536b774
Justyna Ilczuk <att@atte.ro>
2015-05-30 08:17:37 -0700

    Base app structure

---
+++ tags/tags/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Library',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('url', models.URLField()),
+                ('name', models.SlugField(max_length=80)),
+                ('description', models.CharField(blank=True, max_length=256)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('text', models.CharField(unique=True, max_length=64)),
+            ],
+        ),
+    ]
---
+++ tags/tags/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    text = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return '[id: {id}, text: {text}]'.format(id=self.id, text=self.text)
+
+
+
+class Library(models.Model):
+    url = models.URLField()
+    name = models.SlugField(max_length=80)
+    description = models.CharField(max_length=256, blank=True)
+
+
+    def __str__(self):
+        return '[id: {id}, name: {name}]'.format(id=self.id, name=self.name)
